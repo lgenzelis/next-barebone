13182886f02fdafbd371276235c5f3757e658837
Lucas Genzelis <lucas.genzelis@gmail.com>
2021-03-05 22:44:24 -0300

    added some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
@@ -22,13 +22,17 @@ module.exports = {
     'plugin:prettier/recommended', // show prettier errors as ESLint errors. This should be the last configuration in the extends array.
   ],
   rules: {
-    'eqeqeq': 'error',
+    curly: ['error', 'multi-line'],
+    eqeqeq: 'error',
+    'no-throw-literal': 'error',
+    'require-await': 'error',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
     'react-hooks/rules-of-hooks': 'error', // checks rules of Hooks
     'react-hooks/exhaustive-deps': 'error', // checks effect dependencies
     'react/prop-types': 'off', // this rules conflicts with the use of React.FC<props>
     'react/display-name': 'off',
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-non-null-assertion': 'error',
     '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'all', argsIgnorePattern: '^_' }],
     '@typescript-eslint/restrict-plus-operands': 'error',
@@ -41,5 +45,7 @@ module.exports = {
         'newlines-between': 'always',
       },
     ],
+    'import/group-exports': 'error',
+    'import/exports-last': 'error',
   },
-};
+}
