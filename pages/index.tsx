071a2ba913812b393f366478259330c65acce8cb
Lucas Genzelis <lucas.genzelis@gmail.com>
2021-03-05 22:49:21 -0300

    testing husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Homepage = () => {
 
   useEffect(() => {
     const intervalId = setInterval(() => setNMsgs((n) => (n + 1) % 5), 1500)
+
     return () => clearInterval(intervalId)
   }, [])
 
